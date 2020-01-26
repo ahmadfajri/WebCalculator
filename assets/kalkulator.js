8842ee54262203fbe61424e7cbf24a72c874c40a
ahmadfajri <ahmadfajri.fajri6@gmail.com>
2020-01-26 08:55:28 +0700

    menambahkan keterangan komentar sesuai fungsinya

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -1,37 +1,42 @@
+// Menyiapkan variabel awal
 const calculator = {
-  displayNumber: '0',
+  displayNumber: "0",
   operator: null,
   firstNumber: false,
   waitingForSecondNumber: false
 };
 
+// fungsi untuk update display
 function updateDisplay() {
   document.querySelector("#displayNumber").innerText = calculator.displayNumber;
 }
 
+// fungsi untuk mereset calculator
 function clearCalculator() {
-  calculator.displayNumber = '0';
+  calculator.displayNumber = "0";
   calculator.operator = null;
   calculator.firstNumber = false;
   calculator.waitingForSecondNumber = false;
 }
 
+// fungsi untuk inputan
 function inputDigit(digit) {
-  if (calculator.displayNumber === '0') {
+  if (calculator.displayNumber === "0") {
     calculator.displayNumber = digit;
   } else {
     calculator.displayNumber += digit;
   }
 }
 
+// mengambil seluruh elemen button
 const buttons = document.querySelectorAll(".button");
+// lakukan perulangan untuk menyesuaikan elemen target
 for (let button of buttons) {
-  button.addEventListener('click', function (event) {
-
+  button.addEventListener("click", function(event) {
     //mendapatkan objek elemen yang diklik
     const targetEl = event.target;
 
-    if (targetEl.classList.contains('clear')) {
+    if (targetEl.classList.contains("clear")) {
       clearCalculator();
       updateDisplay();
       return;
@@ -40,4 +45,4 @@ for (let button of buttons) {
     inputDigit(targetEl.innerText);
     updateDisplay();
   });
-};
+}
